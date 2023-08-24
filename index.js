ad246c02d6252c0a1070ed6020149921cee3dd2b
Steve <hytteve@gmail.com>
2023-08-24 13:16:23 +0800

    Trying ReactJS

---
+++ index.js
@@ -0,0 +1,21 @@
+// import fetch from "node-fetch";
+// import fs from "fs";
+// async function query(filename) {
+//     const data = fs.readFileSync(filename);
+//     const response = await fetch(
+//         "https://api-inference.huggingface.co/models/google/vit-base-patch16-224",
+//         {
+//             headers: { Authorization: `Bearer ${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}` },
+//             method: "POST",
+//             body: data,
+//         }
+//     );
+//     const result = await response.json();
+//     return result;
+// }
+// query("cat.jpeg").then((response) => {
+//     console.log(JSON.stringify(response));
+// });
+// var Output = document.getElementById("Output");
+// var OutputLine = "<p>" + JSON.stringify(response) + "</p>";
+// Output.innerHTML += OutputLine;
